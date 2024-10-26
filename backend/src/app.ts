887f9cdbcdb9b180dad526ef49709a587dc6115f
pj <Pinjiang1220@gmail.com>
2024-10-27 03:56:28 +0800

    docs: create initial api doc

---
+++ backend/src/app.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import { join } from 'path';
+import swaggerUi from 'swagger-ui-express';
+import fs from 'fs';
+import YAML from 'yaml';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Swagger API doc
+const file  = fs.readFileSync(join(__dirname, '../docs/swagger.yaml'), 'utf8')
+const swaggerDocument = YAML.parse(file)
+
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
