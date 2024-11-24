003343aba6095e3f22858ae72393a43d12f57d1c
chuang091 <thomas@ultralytics.com>
2024-11-24 18:08:38 +0800

    feat(frontend): Add next-auth client module
    - 地圖套件更新 (Nextjs 專門的leaflet)
    - 地圖改用client渲染
    - 用MapBox底圖
    - GitHub Auth

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import MapWrapper from '../components/mapWrapper';
 
 export default function Page() {
-    return <h1>Hello, Next.js!</h1>
+    return (
+        <div>
+            <h1>Hello, Next.js!</h1>
+            <MapWrapper />
+        </div>
+    );
   }
