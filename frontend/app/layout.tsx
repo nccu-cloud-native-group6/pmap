56b9c4fae3c06e6bf93d6827b28413e3912ddd1e
chuang091 <thomas@ultralytics.com>
2024-11-24 18:55:13 +0800

    chore(frontend): Update .env.example and add NEXTAUTH_SECRET

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,21 +1,13 @@
 'use client';
 
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { SessionProvider } from 'next-auth/react';
-import { Session } from 'next-auth';
 
-export default function RootLayout({
-    children,
-    session,
-}: {
-    children: React.ReactNode;
-    session?: Session; // 可選屬性，用於提供初始化的會話
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
     return (
         <html lang="en">
             <body>
-                {/* 包裹 SessionProvider 確保子組件可以訪問身份驗證上下文 */}
-                <SessionProvider session={session}>
+                <SessionProvider>
                     {children}
                 </SessionProvider>
             </body>
