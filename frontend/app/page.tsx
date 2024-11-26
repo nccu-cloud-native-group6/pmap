468552e27d34eb067658fe3919b2b4152537ebbc
chuang091 <thomas@ultralytics.com>
2024-11-26 22:38:57 +0800

    chore(frontend): Init tailwind.config.js and package.json

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import MapWrapper from '../components/mapWrapper';
+import { Button } from "@nextui-org/react";
 
 export default function Page() {
     return (
         <div>
             <h1>Hello, Next.js!</h1>
+            <div><Button color="primary">Hello NextUI</Button></div>
             <MapWrapper />
         </div>
     );
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+const { nextui } = require("@nextui-org/react");
+
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/react/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [nextui()],
+};
