90467e7966d1fd879acc39ccc3144a8233b905bc
yihsiu <yschiu880707@gmail.com>
2024-12-26 20:33:52 +0800

    feat: add notification server

---
+++ notificationServer/socket.js
@@ -0,0 +1,38 @@
+const http = require('http');
+const socketIo = require('socket.io');
+const mqtt = require('mqtt');
+
+const server = http.createServer((req, res) => { });
+const io = socketIo(server);
+let mqttClient = mqtt.connect("mqtt://localhost:1883");
+
+mqttClient.on("connect", () => {
+    mqttClient.subscribe("WEBSOCKET", (err) => { });
+    console.log("mqtt connected");
+});
+
+io.on('connection', (socket) => {
+    socket.on('message', (data) => {
+        if (JSON.parse(data).hasOwnProperty('userId')) {
+            socket.join(JSON.parse(data).userId.toString());
+            console.log("add user to room: " + JSON.parse(data).userId.toString());
+        }
+    });
+});
+
+// 啟動伺服器
+const PORT = 3002;
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
+
+mqttClient.on("message", (topic, message) => {
+    data = JSON.parse(message.toString());
+    if (data.length > 0) {
+        console.log(data);
+        data.forEach(element => {
+            console.log("send to user: " + element.userId.toString());
+            io.to(element.userId.toString()).emit("message", JSON.stringify(element));
+        });
+    }
+});
