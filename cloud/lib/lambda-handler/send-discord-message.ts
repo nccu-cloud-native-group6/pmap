d18639a683d53e0601c73d1d52159ca3546b309f
pj <Pinjiang1220@gmail.com>
2024-12-28 16:55:25 +0800

    feat: add send-dc lambda support lambda on-failure event

--- cloud/lib/lambda-handler/send-discord-message.ts
+++ cloud/lib/lambda-handler/send-discord-message.ts
@@ -1,28 +1,92 @@
 /**
  * A lambda for sending a message to Discord
-*/
+ */
 
-export const handler = async (event: any): Promise<any> => {
-  console.log("Recv event: ", JSON.stringify(event, null, 2));
- 
-  const resp = event.responsePayload;
-  let message = resp.message;
-  let statusCode = Number(resp.statusCode);
-  
-  await sendMsgToDiscord(process.env.DISCORD_WEBHOOK_URL!, message, statusCode);
+interface DiscordEmbed {
+  title?: string;
+  description?: string;
+  fields?: {
+    name: string;
+    value: string;
+    inline?: boolean;
+  }[];
+  color?: number;
+  footer?: {
+    text: string;
+  };
 }
 
-async function sendMsgToDiscord(webhookUrl: string, message: string, statusCode: number) {
-  console.log(`Sending message to Discord: ${message}`);
-  const content = `${statusToEmoji(statusCode)} ${message}`; 
+export const handler = async (event: any): Promise<any> => {
+  console.log('Recv event: ', JSON.stringify(event, null, 2));
+
+  let content = undefined;
+  let embeds = undefined;
+
+  // The first event format will have responsePayload.message
+  if (event.responsePayload.message) {
+    const resp = event.responsePayload;
+    const message = resp.message;
+    const statusCode = Number(resp.statusCode);
+    content = `${statusToEmoji(statusCode)} ${message}`;
+  } else {
+    // The other event is from lambda-destination on-failure
+    embeds = await parsedOnFailureMessage(event);
+  }
+  await sendMsg(process.env.DISCORD_WEBHOOK_URL!, content, embeds);
+};
+
+function formatCodebox(content: string, maxLength = 1024) {
+  // Discord 限制每個 field 最多 1024 字元
+  const truncatedContent = content.slice(0, maxLength);
+  return `\`\`\`${truncatedContent}\`\`\``; // Wrap in codeblock
+}
+
+async function parsedOnFailureMessage(event: any) {
+  const functionArn = event.requestContext.functionArn;
+  const functionName = functionArn.split(':').at(-2);
+  const errorMessage = event.responsePayload.errorMessage;
+  const errorTrace = event.responsePayload.trace.join('\n');
+  const requestId = event.requestContext.requestId;
+
+  return [
+    {
+      title: `🚨 Lambda Error: ${functionName}`,
+      description: `**Message:** ${errorMessage}`,
+      color: 0xff0000, // 紅色
+      fields: [
+        {
+          name: 'Function',
+          value: formatCodebox(functionName),
+        },
+        {
+          name: 'Request ID',
+          value: formatCodebox(requestId),
+          inline: true,
+        },
+        {
+          name: 'Trace',
+          value: formatCodebox(errorTrace),
+          inline: false,
+        },
+      ],
+    },
+  ];
+}
+
+async function sendMsg(
+  webhookUrl: string,
+  content?: string,
+  embeds?: DiscordEmbed[],
+) {
   const response = await fetch(webhookUrl, {
-    method: "POST",
+    method: 'POST',
     body: JSON.stringify({
       content: content,
-      username: "Pmap 小精靈",
-      avatar_url: "https://i.imgur.com/i1KO4EQ.png",
+      embeds: embeds,
+      username: 'Pmap 小精靈',
+      avatar_url: 'https://i.imgur.com/i1KO4EQ.png',
     }),
-    headers: { "Content-type": "application/json; charset=UTF-8" },
+    headers: { 'Content-type': 'application/json; charset=UTF-8' },
   });
   return response;
 }
@@ -30,10 +94,10 @@ async function sendMsgToDiscord(webhookUrl: string, message: string, statusCode:
 function statusToEmoji(statusCode: number) {
   switch (statusCode) {
     case 200:
-      return ":white_check_mark:";
+      return ':white_check_mark:';
     case 500:
-      return ":x:";
+      return ':x:';
     default:
-      return ":sound:";
+      return ':sound:';
   }
 }
--- cloud/lib/local/test/send-discord-message-test.ts
+++ cloud/lib/local/test/send-discord-message-test.ts
@@ -4,11 +4,63 @@ import { loadEnv } from '../config';
 loadEnv();
 
 async function testHandler() {
+  // Event format 1:
   try {
-    await handler({ responsePayload: {
-      statusCode: 200,
-      message: '測試',
-    } });
+    await handler({
+      responsePayload: {
+        statusCode: 200,
+        message: '測試',
+      },
+    });
+  } catch (error) {
+    console.error('Test failed:', error);
+  }
+
+  // Event format 2 (lambda-destionation on-failure):
+  try {
+    await handler({
+      version: '1.0',
+      timestamp: '2024-12-28T07:13:09.891Z',
+      requestContext: {
+        requestId: 'd504cbea-eb99-4601-b61c-d594fc6fea3d',
+        functionArn:
+          'arn:aws:lambda:ap-northeast-1:203918887121:function:CloudStack-fetchweatherC769A7ED-8gf8MnoABeHp:$LATEST',
+        condition: 'RetriesExhausted',
+        approximateInvokeCount: 3,
+      },
+      requestPayload: {
+        version: '0',
+        id: 'f6b5da9e-f839-4ce8-8951-41d5c4497427',
+        'detail-type': 'Scheduled Event',
+        source: 'aws.events',
+        account: '203918887121',
+        time: '2024-12-28T07:10:00Z',
+        region: 'ap-northeast-1',
+        resources: [
+          'arn:aws:events:ap-northeast-1:203918887121:rule/CloudStack-Rule4C995B7F-gpnzOVlTsOgl',
+        ],
+        detail: {},
+      },
+      responseContext: {
+        statusCode: 200,
+        executedVersion: '$LATEST',
+        functionError: 'Unhandled',
+      },
+      responsePayload: {
+        errorType: 'Error',
+        errorMessage: 'Failed to get address',
+        trace: [
+          'Error: Failed to get address',
+          '    at /var/task/index.js:14328:15',
+          '    at Array.map (<anonymous>)',
+          '    at addAddress (/var/task/index.js:14324:14)',
+          '    at fetchCwaData (/var/task/index.js:14285:44)',
+          '    at process.processTicksAndRejections (node:internal/process/task_queues:95:5)',
+          '    at async main (/var/task/index.js:14274:17)',
+          '    at async Runtime.handler (/var/task/index.js:14271:10)',
+        ],
+      },
+    });
   } catch (error) {
     console.error('Test failed:', error);
   }
