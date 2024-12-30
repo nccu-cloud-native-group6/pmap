4d3e26c76fc1a11573f0c81b74963a64e49d2b89
Lu-weiting <runeeld23@gmail.com>
2024-12-30 23:36:52 +0800

    Feat: add prometheus client

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -16,12 +16,20 @@ import { multerErrorHandling } from './Middlewares/multer.js';
 import { notificationService } from './Infrastructure/Service/notificationService.js';
 import cors from 'cors';
 import { reportService } from './Infrastructure/Service/reportService.js';
-
+import promClient from 'prom-client';
 const app = express();
 const port = process.env.BACKEND_PORT;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+// 收集預設系統指標
+const collectDefaultMetrics = promClient.collectDefaultMetrics;
+collectDefaultMetrics();
 
+const httpRequestCount = new promClient.Counter({
+  name: 'http_requests_total',
+  help: 'Total number of HTTP requests',
+  labelNames: ['method', 'path', 'status_code'],
+});
 // Swagger API doc
 const file = fs.readFileSync(
   path.resolve(__dirname, '../docs/swagger.yaml'),
@@ -31,6 +39,15 @@ const swaggerDocument = YAML.parse(file);
 
 app.use(cors());
 app.use(express.json());
+// Middleware：紀錄每次request
+app.use((req, res, next) => {
+  res.on('finish', () => {
+    const { method, path } = req;
+    const { statusCode } = res;
+    httpRequestCount.labels(method, path, statusCode.toString()).inc();
+  });
+  next();
+});
 app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api/weather', weatherRouter);
 app.use('/api/auth', authRouter);
@@ -40,6 +57,11 @@ app.use('/api/users/:userId/subscriptions', subscriptionRouter);
 
 app.get('/api/health', (req: Request, res: Response) => {
   res.send('Hello');
+});
+// /metrics endpoint
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', promClient.register.contentType);
+  res.end(await promClient.register.metrics());
 });
 app.use(multerErrorHandling);
 app.use(errorHandler);
